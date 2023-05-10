08148afa7cdea382829a1fb88dcab80e5275060e
sebastianmend <sebastianfelipemendietalima@gmail.com>
2023-05-10 12:45:42 -0500

    semana5

--- ejemplos-relaciones-clases/EjemplosRelacionesDos/src/paquete1/Ejecutor.java
+++ ejemplos-relaciones-clases/EjemplosRelacionesDos/src/paquete1/Ejecutor.java
@@ -16,16 +16,18 @@ public class Ejecutor {
         Barrio barrio1 = new Barrio("San Sebastián");
         Barrio barrio2 = new Barrio("Sucre");
         
-        Persona persona1 = new Persona("René Elizalde", "1100909909", barrio1);
+        Persona persona1 = new Persona("René Elizalde", 
+                "1100909909", barrio1);
         
         System.out.printf("El barrio de la persona %s (%s) es: %s\n",
                 persona1.obtenerNombre(),
+                persona1.obtenerCedula(),
                 persona1.obtenerBarrio().obtenerNombre());
         
         persona1.establecerBarrio(barrio2);
         
-        System.out.printf("El barrio de la persona %s (%s) es: %s\n",
-                persona1.obtenerNombre(),
+        System.out.printf("El barrio de la persona %s  es: %s\n",
+                persona1.obtenerNombre().toUpperCase(),
                 persona1.obtenerBarrio().obtenerNombre());
     }
 }
--- ejemplos-relaciones-clases/EjemplosRelacionesDos/src/paquete2/Ejecutor02.java
+++ ejemplos-relaciones-clases/EjemplosRelacionesDos/src/paquete2/Ejecutor02.java
@@ -12,7 +12,47 @@ package paquete2;
 public class Ejecutor02 {
     
     public static void main(String[] args) {
+        Ciudad ciudad1 = new Ciudad("Loja");
+        Ciudad ciudad2 = new Ciudad("Cuenca");
+        Ciudad ciudad3 = new Ciudad("Guayaquil");
+        
+        Barrio barrio1 = new Barrio("el Valle", ciudad1);
+        Barrio barrio2 = new Barrio("Central", ciudad2);
+        Barrio barrio3 = new Barrio("Centenario", ciudad3);
+        
+        Persona persona1 = new Persona("Maria Del Valle", "110121219909", barrio1);
+        Persona persona2 = new Persona("Jean Villavicencio", "11232309", barrio2);
+        Persona persona3 = new Persona("Juan Zambrano", "12343409", barrio3);
+        
+        System.out.println("------------BARRIOS DE PERSONAS----------------");
+        System.out.printf("El barrio de la persona %s es: %s Y pertenece a la "
+                + "ciudad: %s\n",
+                persona1.obtenerNombre(),
+                persona1.obtenerBarrio().obtenerNombre(),
+                persona1.obtenerBarrio().obtenerCiudad().obtenerNombre());
+        System.out.println("------------------------------------------");
+        
+        
+        
+        System.out.printf("El barrio de la persona %s es: %s Y pertenece a la "
+                + "ciudad: %s\n",
+                persona2.obtenerNombre(),
+                persona2.obtenerBarrio().obtenerNombre(),
+                persona2.obtenerBarrio().obtenerCiudad().obtenerNombre());
+        System.out.println("------------------------------------------");
+        
+        
+        
+        System.out.printf("El barrio de la persona %s es: %s Y pertenece a la "
+                + "ciudad: %s\n",
+                persona3.obtenerNombre(),
+                persona3.obtenerBarrio().obtenerNombre(),
+                persona3.obtenerBarrio().obtenerCiudad().obtenerNombre());
+        
         // 1. Crear una persona de Barrio el Valle de la ciudad de Loja
+        
+        
+        
         // 2. Crear una persona del Barrio Central de la ciudad de Cuenca
         // 3. Crear una persona del Barrio Centenario de la ciuda de Guayaquil
         
